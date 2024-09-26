70927bd5cbee6cf8bc3195d12e3c84a1d88a11b1
sktanui <shadrackkemei2019@gmail.com>
2024-09-26 16:20:49 +0300

    looping lab complete

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
 // Code your solutions in this file
+// for (let age=30; age<40; age++) {
+//     console.log(I'm ${age} years old. Happy birthday to me!);
+    
+// }
+// const gifts=["teddy bear", "drone", "doll"];
+
+// function wrapGifts(gifts) {
+//     for (let i=0; i<gifts.length; i++) {
+//         console.log(Wrapped ${gifts[i]} and added a bowl!);
+//         debugger;
+//     }
+//     return gifts;
+// }
+// wrapGifts(gifts);
+
+function writeCards(names) {
+    const messages = [];
+  
+    for (const name of names) {
+      const message = (`Thank you, ${name}, for the wonderful surprise gift!`);
+      messages.push(message);
+    }
+  
+    return messages;
+  }
+  
+  function countDown(number) {
+    for (let i = number; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  module.exports = { writeCards, countDown };
